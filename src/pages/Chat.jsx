df60eac167a0c8694758b8cf6d7fe60725c86713
Lai-Star <tsangyu.lai0920@gmail.com>
2024-06-24 09:52:43 +0900

    fix sidebar

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -2,17 +2,12 @@ import React from 'react';
 import Chatdetail from '../components/messages/Chatdetail';
 import Chatlist from '../components/messages/Chatlist';
 import Messagebox from '../components/messages/Messagebox';
-import Sidebar from '../components/Sidebar';
 
 export default function Chat() {
   return (
     <div className="min-h-screen flex flex-col">
       {/* Chatting */}
       <div className="flex flex-1 flex-row justify-between bg-white">
-        {/* Left Sidebar */}
-        <Sidebar />
-        {/* End Left Sidebar */}
-
         {/* Chat list */}
         <Chatlist />
         {/* End Chat list */}
--- src/pages/Meeting.jsx
+++ src/pages/Meeting.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import MainScreen from '../components/meeting/MainScreen';
-import Sidebar from '../components/Sidebar';
 import MeetinDetail from '../components/meeting/MeetinDetail';
 
 export default function Meeting() {
@@ -8,10 +7,6 @@ export default function Meeting() {
     <div className="min-h-screen flex flex-col">
       {/* Chatting */}
       <div className="flex flex-1 flex-row justify-between bg-white">
-        {/* Left Sidebar */}
-        <Sidebar />
-        {/* End Left Sidebar */}
-
         {/* Meeting Main Screen */}
         <MainScreen />
         {/* End Meeting Main Screen */}
