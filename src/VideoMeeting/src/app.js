7afb1c2e235d76d5d601d5e150fb9735616f541e
Lai-Star <tsangyu.lai0920@gmail.com>
2024-06-30 17:36:10 +0900

    add video meeting

---
+++ src/VideoMeeting/src/app.js
@@ -0,0 +1,47 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon');
+
+// const mongoose = require('mongoose');
+// const cors = require('cors');
+// require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+// app.use(cors());
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+//MongoDB connection
+// mongoose
+//   .connect(process.env.MONGODB_URI, {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   })
+//   .then(() => console.log('MongoDB connected'))
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+// Models
+// const User = require('./models/User');
+// const Post = require('./models/Post');
+// const Comment = require('./models/Comment');
+
+//Routes
+// app.use('/users', require('./routes/users'));
+// app.use('/posts', require('./routes/posts'));
+// app.use('/comments', require('./routes/comments'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.of('/stream').on('connection', stream);
+
+server.listen(5000);
---
+++ src/VideoMeeting/src/routes/posts.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const Post = require('../models/Post');
+
+// Get all posts
+router.get('/', async (req, res) => {
+  try {
+    const posts = await Post.find().populate('author');
+    res.json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+// Create a new post
+router.post('/', async (req, res) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+  });
+
+  try {
+    const newPost = await post.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
